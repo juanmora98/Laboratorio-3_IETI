19d72c012b1c87f8d9fe76e564fc15b00ba483be
juanmora98 <juan.mora-q@mail.escuelaing.edu.co>
2020-09-03 09:25:35 -0500

    codigo de honor y arreglos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { User } from './components/userComponent/User';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 
+
 function App() {
   return (
     <Router>
